35aaf793ec55527ae43c77cd89a0f58251c5f27e
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-06-03 13:51:47 -0400

    Add `right-margin` to `disable-mouse--bindings-targets`

--- disable-mouse.el
+++ disable-mouse.el
@@ -52,7 +52,7 @@
 (defconst disable-mouse--bindings-modifier-combos
   '("C-" "M-" "S-" "C-M-" "C-S-" "M-S-" "M-C-S-"))
 
-(defconst disable-mouse--bindings-targets '("mode-line" "bottom-divider" "vertical-line"))
+(defconst disable-mouse--bindings-targets '("right-margin" "mode-line" "bottom-divider" "vertical-line"))
 
 (defconst disable-mouse--multipliers '("double" "triple"))
 
